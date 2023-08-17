c444af44598c874d0003eb82ea03270c42139508
Pankaj Jain <pankaj_jain@epam.com>
2023-08-17 14:37:39 +0530

    fetch more result

--- assets/js/search.js
+++ assets/js/search.js
@@ -23,3 +23,50 @@ async function fetchSearchResults() {
         console.error("Error fetching search results:", error);
     }
 }
+
+
+let page = 1;
+const resultsPerPage = 40; // Adjust this as needed
+let loading = false;
+
+async function fetchMoreResults() {
+    if (loading) return;
+
+    loading = true;
+    document.getElementById('loading-indicator').style.display = 'block';
+
+    const searchInput = document.getElementById("shoptop-product-search-field-0");
+    const searchQuery = searchInput.value.replace(/ /g, '+');
+    const apiUrl = `https://flipkart-api.pjain287074.workers.dev/search/${searchQuery}?page=${page}&per_page=${resultsPerPage}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const html = await response.text();
+
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = html;
+
+        const newResults = tempDiv.querySelector('.products.columns-4').innerHTML;
+        const ulElement = document.querySelector('.products.columns-4');
+        ulElement.innerHTML += newResults;
+
+        page++;
+        loading = false;
+        document.getElementById('loading-indicator').style.display = 'none';
+    } catch (error) {
+        console.error("Error fetching more results:", error);
+        loading = false;
+        document.getElementById('loading-indicator').style.display = 'none';
+    }
+}
+
+// Add a scroll event listener to trigger loading more results
+window.addEventListener('scroll', () => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 10) {
+        fetchMoreResults();
+    }
+});
+
+// Initial fetch
+fetchMoreResults();
